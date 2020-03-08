1d87a01efc678d238e7887a6648db6bc1d351075
michaldobiezynski <dobiezynski@gmail.com>
2020-03-08 21:07:55 +0000

    trying to fetch the data for the reducer state via action (axios)

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -9,8 +9,14 @@ class PostList extends React.Component {
   }
 
   render() {
+    console.log(this.props.posts);
     return <div>Post List</div>;
   }
 }
 
-export default connect(null, { fetchPosts })(PostList);
+const mapStateToProps = state => {
+  console.log(state);
+  return { posts: state.posts };
+};
+
+export default connect(mapStateToProps, { fetchPosts })(PostList);
